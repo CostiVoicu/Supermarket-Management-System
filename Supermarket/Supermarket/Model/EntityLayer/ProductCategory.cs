04813bd57145a3db5503c308c64fff1f6f566e05
Constantin Voicu <voicu.constantin@outlook.com>
2024-05-08 23:16:25 +0300

    Add product category entity class

---
+++ Supermarket/Supermarket/Model/EntityLayer/ProductCategory.cs
@@ -0,0 +1,34 @@
+﻿using Supermarket.Core;
+
+namespace Supermarket.Model.EntityLayer
+{
+    public class ProductCategory : ObservableObject
+    {
+        private int? categoryId;
+        public int? CategoryId
+        {
+            get
+            {
+                return categoryId;
+            }
+            set
+            {
+                categoryId = value;
+                OnPropertyChanged("id");
+            }
+        }
+        private string categoryName;
+        public string CategoryName
+        {
+            get
+            {
+                return categoryName;
+            }
+            set
+            {
+                categoryName = value;
+                OnPropertyChanged("name");
+            }
+        }
+    }
+}
