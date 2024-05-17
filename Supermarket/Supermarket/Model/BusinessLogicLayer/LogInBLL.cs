385dcfed7660870b33664ffafd04e0da5568bfb2
Constantin Voicu <voicu.constantin@outlook.com>
2024-05-17 20:58:29 +0300

    Add log in functionality Add LogInBLL class Delete Start layer and sign up layer

---
+++ Supermarket/Supermarket/Model/BusinessLogicLayer/LogInBLL.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Supermarket.Model.BusinessLogicLayer
+{
+    public class LogInBLL
+    {
+        private SupermarketDBEntities1 context = new SupermarketDBEntities1();
+        public ObservableCollection<user> UsersList { get; set; }
+        public string ErrorMessage { get; set; }
+
+        public void AddMethod(object obj)
+        {
+            user usr = obj as user;
+            if (usr != null)
+            {
+                if (string.IsNullOrEmpty(usr.name))
+                {
+                    ErrorMessage = "User name is empty!";
+                }
+                else
+                {
+                    if (usr.user_type_id == 1)
+                    {
+                        context.insert_user(usr.name, usr.password, "Admin");
+                    }
+                    else
+                    {
+                        context.insert_user(usr.name, usr.password, "Cashier");
+                    }
+                    context.SaveChanges();
+                    //TO DO: maybe you should assign the id
+                    UsersList.Add(usr);
+                    ErrorMessage = "";
+                }
+            }
+        }
+        public ObservableCollection<user> GetAllUsers()
+        {
+            List<user> users = context.users.ToList();
+            ObservableCollection<user> result = new ObservableCollection<user>();
+            foreach (user user in users)
+            {
+                result.Add(user);
+            }
+            return result;
+        }
+    }
+}
--- Supermarket/Supermarket/ViewModel/LogInViewModel.cs
+++ Supermarket/Supermarket/ViewModel/LogInViewModel.cs
@@ -1,12 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Supermarket.Core;
+using Supermarket.Model;
+using Supermarket.Model.BusinessLogicLayer;
+using Supermarket.Services;
+using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Supermarket.ViewModel
 {
     public class LogInViewModel : Core.ViewModel
     {
+        private LogInBLL _loginBll;
+
+        private INavigationService _navigation;
+        public INavigationService Navigation
+        {
+            get => _navigation;
+            set
+            {
+                _navigation = value;
+                OnPropertyChanged();
+            }
+        }
+        public LogInViewModel(INavigationService navService)
+        {
+            _loginBll = new LogInBLL();
+            Navigation = navService;
+            UsersList = _loginBll.GetAllUsers();
+        }
+
+        public ObservableCollection<user> UsersList
+        {
+            get => _loginBll.UsersList;
+            set => _loginBll.UsersList = value;
+        }
+
+        private string _logInUsername;
+        public string LogInUsername
+        {
+            get => _logInUsername; 
+            set { _logInUsername = value; OnPropertyChanged(); }
+        }
+        private string _logInPassword;
+        public string LogInPassword
+        {
+            get => _logInPassword;
+            set { _logInPassword = value; OnPropertyChanged(); }
+        }
+
+        private bool _isAdmin;
+        public bool IsAdmin
+        {
+            get => _isAdmin;
+            set { _isAdmin = value; OnPropertyChanged(); }
+        }
+        private bool _isCashier;
+        public bool IsCashier
+        {
+            get => _isCashier;
+            set { _isCashier = value; OnPropertyChanged(); }
+        }
+        public ObservableCollection<user> UserList
+        {
+            get => _loginBll.UsersList;
+            set => _loginBll.UsersList = value;
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get => _errorMessage;
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged("ErrorMessage");
+            }
+        }
+        public void LogInUser()
+        {
+            var validUser = UsersList.FirstOrDefault(u => u.name == LogInUsername && u.password == LogInPassword);
+            if (validUser == null) { return; }
+            if (_isAdmin && validUser.user_type_id == 1)
+            {
+                Navigation.NavigateTo<AdminViewModel>();
+            }
+            else if (_isCashier && validUser.user_type_id == 2)
+            {
+                Navigation.NavigateTo<CashierViewModel>();
+            }
+        }
+
+        private ICommand _logInCommand;
+        public ICommand LogInCommand
+        {
+            get
+            {
+                if (_logInCommand == null)
+                {
+                    _logInCommand = new RelayCommand(o => { LogInUser(); }, o => true);
+                }
+                return _logInCommand;
+            }
+        }
     }
 }
